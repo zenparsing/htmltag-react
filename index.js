b19d4b8cc253d9d91d9857baabf0a2f85e5e21c0
Kevin Smith <KevinH.Smith@coxinc.com>
2017-06-15 11:51:00 -0400

    Remove special "using" logic (moved upstream to htmltag)

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ var htmlTag = require('htmltag');
 var React = require('react');
 
 function htmlTagReact(type, props, children) {
-  if (typeof props.using === 'function') {
-    var p = {};
-    Object.keys(props).forEach(function(key) {
-      if (key !== 'using') {
-        p[key] = props[key];
-      }
-    });
-    type = props.using;
-    props = p;
-  }
   var args = [type, props];
   if (children.length > 0) {
     args = args.concat(children);
